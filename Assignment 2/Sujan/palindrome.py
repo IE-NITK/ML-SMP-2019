a2606bcb8eb8bbb0acda31911ddba85e205b1c33
shreya <sujanreddy242@gmail.com>
2019-05-08 10:26:53 +0530

    Assignment 2 -Sujan

--- Assignment 2/Sujan/palindrome.py
+++ Assignment 2/Sujan/palindrome.py
@@ -1,6 +1,6 @@
-a=input("Enter");f=-1;
+a=input("Enter");f=0;
 for i in  range((int)(len(a)/2)):
-    if a[i]==a[len(a)-i-1]:
-        f=0
+    if a[i]!=a[len(a)-i-1]:
+        f=-1
         break
 print (f)
