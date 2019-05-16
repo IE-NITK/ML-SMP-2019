82d1e0c769e6e0fcc630f71f0835347d3c6163c7
DhruvTh <50106469+DhruvTh@users.noreply.github.com>
2019-05-16 18:29:36 +0530

    Update Prime.py

--- Assignment 3/Dhruv/Prime.py
+++ Assignment 3/Dhruv/Prime.py
@@ -1,12 +1,7 @@
 a=int(input("Enter upper limit\n"))
-b=[]
-c={i for i in range(2,a)}
-j=0
-for i in range(2,a):
-	b.append(i)
-for i in range(2,a):
-	for j in range(0,len(b)):
-		if(b[j]%i==0):
-			if(b[j]!=i):
-				 c.discard(b[j])
-print("Prime numbers upto that limit ",c)
+j=2
+b={i for i in range(2,a)}
+for i in range(2,10):
+	b=b.difference({ i*j for j in range(2,int(a/j))})
+print("Prime number upto that limit ",b)
+	
